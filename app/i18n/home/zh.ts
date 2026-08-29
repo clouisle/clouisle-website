9bfc50d3bf6582021bb3f016eb2387455bec88d4
YunHai <yunhai@yhnotes.com>
2026-08-29 19:52:32 +0800

    i18n: rename footer group 公司 to 组织

--- app/i18n/home/zh.ts
+++ app/i18n/home/zh.ts
@@ -132,7 +132,7 @@ export const zh: HomeTranslations = {
     groups: [
       { title: "产品", links: ["工作版", "发布说明", "Windows", "学生", "报告"] },
       { title: "资源", links: ["帮助", "状态", "隐私", "使用条款", "安全", "信任中心"] },
-      { title: "公司", links: ["关于我们", "招聘", "通讯", "早期用户计划"] },
+      { title: "组织", links: ["关于我们", "招聘", "通讯", "早期用户计划"] },
       { title: "联系", links: ["X", "LinkedIn", "Instagram"] },
     ],
   },
